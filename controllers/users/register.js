8e41942b506ab987596cc45c28a139aa9652202c
Bellmere <therise00@gmail.com>
2023-04-04 10:07:32 +0300

    add sal

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,4 +1,6 @@
 const { Conflict } = require("http-errors");
+const bcrypt = require('bcryptjs');
+
 const { User } = require("../../models");
 
 const register = async (req, res, next) => {
@@ -8,7 +10,8 @@ const register = async (req, res, next) => {
     if (user) {
       throw new Conflict(`${email} in use`);
     }
-    const result = await User.create({ name, email, password });
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    const result = await User.create({ name, email, password: hashPassword });
     res.statu(201).json(result);
   } catch (error) {
     next(error);
