f49fd64513435976bcf1ec5a8ae818872d48eedd
Bellmere <therise00@gmail.com>
2023-04-09 21:35:10 +0300

    Add Logic for avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
-const { auth } = require("../../middlewares");
+const { auth, ctrlWrapper, upload } = require("../../middlewares");
 const { currentUser: ctrl } = require("../../controllers");
-const { getCurrent } = ctrl;
+const { getCurrent, updateAvatar } = ctrl;
 
 const router = express.Router();
 
-router.get("/current", auth, getCurrent);
+router.get("/current", auth, ctrlWrapper(getCurrent));
+
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  ctrlWrapper(updateAvatar)
+);
 
 module.exports = router;
